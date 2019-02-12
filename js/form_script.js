4a048dcca2266e8c335c6bf78a5599ae6f3986dc
Luis Garcia <garcia50@yahoo.com>
2019-02-12 17:44:36 -0500

    Display the color options that match the design selected in the "Design" menu.

--- js/form_script.js
+++ js/form_script.js
@@ -15,3 +15,18 @@ $('#title').on('change', function(e){
     $('#other-title').hide();
   }
 });
+
+
+$('#design').on('change', function(e){
+  $('#color option').show();
+  if (e.target.value == 'js puns') {
+    $('#color option').filter(':eq(3), :eq(4), :eq(5)').hide();
+    $('#color>option:eq(0)').prop('selected', true);
+  } else if (e.target.value == 'heart js') {
+    $('#color option').filter(':eq(0), :eq(1), :eq(2)').hide();
+    $('#color>option:eq(3)').prop('selected', true);
+  } else {
+    $('#color option').show();
+    $('#color>option:eq(0)').prop('selected', true);
+  }
+});
