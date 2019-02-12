af824ea2e5805d8f18152183ef6f2d673eb36cb0
Luis Garcia <garcia50@yahoo.com>
2019-02-12 15:32:52 -0500

    Clean up sapcing and remove unwanted code

--- js/form_script.js
+++ js/form_script.js
@@ -1,23 +1,8 @@
 // const nameField = $('#name');
-$('#name').focus();
-
-
 const $otherTitle = $('<input type="text" id="other-title" placeholder="Your Job Role">');
 
-const $otherOption = $("#title")
 
-// $('#title').on('click', 'option', function(){
-//   console.log(this)
-//   $(this).append($otherTitle);
-//   // $('#title option')
-// });
 
-// $('#title option');
 
-$('.job-role').append($otherTitle);
 
-// $('#title option').append($otherTitle);
 
-// $('#title input').show();
-// $('#title input').hide();
-const jobRole = $('.job-role')
