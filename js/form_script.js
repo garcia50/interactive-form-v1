50e4ced9a85f7344500f598ebfc43ccbd979d105
Luis Garcia <garcia50@yahoo.com>
2019-02-12 15:33:31 -0500

    Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.

--- js/form_script.js
+++ js/form_script.js
@@ -3,6 +3,15 @@ const $otherTitle = $('<input type="text" id="other-title" placeholder="Your Job
 
 
 
+$('#name').focus();
 
 
 
+$('#title').on('change', function(e){
+  if (e.target.value === 'other') {
+    $(this).parent().append($otherTitle);
+    $('#other-title').show();
+  } else {
+    $('#other-title').hide();
+  }
+});
