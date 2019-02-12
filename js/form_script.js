a8eccd37d48593e75733f0ccf801f3cfa0038663
Luis Garcia <garcia50@yahoo.com>
2019-02-12 17:46:41 -0500

    Remove dead code and clean spacing.

--- js/form_script.js
+++ js/form_script.js
@@ -1,10 +1,7 @@
-// const nameField = $('#name');
 const $otherTitle = $('<input type="text" id="other-title" placeholder="Your Job Role">');
 
 
-
 $('#name').focus();
-
 
 
 $('#title').on('change', function(e){
